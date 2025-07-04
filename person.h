d1f41fc56721f701fc5cc64eb8796c1a56a24801
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2025-07-04 10:46:53 +0530

    added person.h

---
+++ person.h
@@ -0,0 +1,15 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include<string>
+struct Person {
+    std::string id;
+    std::string name;
+    int age;
+    std::string gender;
+    std::string location;
+    Person() {}
+    Person(std::string i, std::string n, int a, std::string g, std::string l)
+        : id(i), name(n), age(a), gender(g), location(l) {}
+};
+#endif
+
